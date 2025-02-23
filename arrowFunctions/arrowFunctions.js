a8f5e358bd4c96b5d30421babc81e9b16533e08d
Pavel Moskov <pavel.moskov@gmail.com>
2025-02-23 17:05:04 +0100

    Add arrowFunctions practice

---
+++ arrowFunctions/arrowFunctions.js
@@ -0,0 +1,21 @@
+//функция "myFilter" работает аналогично методу массивов "filter"
+
+function myFilter(arr, fn) {
+  let newArr = [];
+  for (let item of arr) {
+    if (fn(item)) {
+      newArr.push(item);
+    }
+  }
+  return newArr;
+}
+
+const numbers = [1, 2, 3, 4, 5];
+
+//использование myFilter для фильтрации четных чисел
+const evenNumbers = myFilter(numbers, (num) => num % 2 === 0);
+console.log(evenNumbers);
+
+//фильтрация чисел больше 3
+const greaterThanThree = myFilter(numbers, (num) => num > 3);
+console.log(greaterThanThree);
